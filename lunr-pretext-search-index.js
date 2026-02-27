c4bbd4d18323521704e9dde603cfdedc88aae268
EGriesenauer <grieseel@eckerd.edu>
2026-02-27 18:51:55 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -16,7 +16,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1.1",
   "title": "Definitions",
-  "body": " Definitions  As we move through Precalculus and Calculus, we will need to be careful with the words that we use. Mathematical writing is characterized by precise and careful language, which allows us to avoid ambiguity and possible misunderstandings. However, we will only avoid misunderstanding if we all know what words mean. Throughout these notes, I will include definitions for the terms that I use, and I need you to make note of these definitions and refer back to them when you see a word you don't recognize. In many ways, mathematics is a language that can describe quantitative or geometric information. Calculus, the focus of these notes, is a language that we can use to describe quantities that change. We will spend a lot of time discussing the rules that govern this language, but it is also crucially important to take the time to learn the words that make up this language.    An expression is a combination of numbers, variables, and operations.    This definition perhaps leads to more questions than it answers. After all, what are the precise, mathematical definitions of numbers, variables, and operations?  For us, in this book, numbers will always refer to real numbers . It is difficult to define real numbers precisely, but you are familiar with them nonetheless. These are numbers that correspond to positions on the number line. Positive real numbers correspond to lengths. All real numbers are positive, negative, or zero. Real numbers include whole numbers, fractions, and irrational numbers. Irrational numbers, like and , are numbers that can't be written as ratios of whole numbers, and their decimal representations are nonterminating and nonrepeating.  A variable is a letter or symbol that is used to represent an unknown or unspecified number. There is some nuance here. In an equation, the variable is unknown and the goal will be to solve for all possible values of the variable that make the equation true. In function notation, the variable is unspecified, and we will replace the variable with various numbers and see how the output of the function changes.  An operation is a rule for transforming one or more numbers into a new number. Most operations act on two numbers, and examples of operations include addition, subtraction, multiplication, and division . Importantly, equality ( ) is not an operation, but instead something called a relation . So, while and are examples of expressions, the equation is not an expression.    An equation is statement that the values of two mathematical expressions are equal.    An equation might always be true, like the equations and , or they might be conditionally true, such as the equation , which is only true if or . It is also possible for an equation to always be false, such as the equation .    An inequality is a statement that compares two expressions using one of the following symbols: (less than), (less than or equal to), (greater than), (great than or equal to), or (not equal).    Just like equations, an inequality might be always true, conditionally true, or always false. For example, the inequality is true for every real number because the square of a real number is always zero or positive. On the other hand, the inequality is only true if the number is between 0 and 1.  "
+  "body": " Definitions  As we move through Precalculus and Calculus, we will need to be careful with the words that we use. Mathematical writing is characterized by precise and careful language, which allows us to avoid ambiguity and possible misunderstandings. However, we will only avoid misunderstanding if we all know what words mean. Throughout these notes, I will include definitions for the terms that I use, and I need you to make note of these definitions and refer back to them when you see a word you don't recognize. In many ways, mathematics is a language that can describe quantitative or geometric information. Calculus, the focus of these notes, is a language that we can use to describe quantities that change. We will spend a lot of time discussing the rules that govern this language, but it is also crucially important to take the time to learn the words that make up this language.  Expression   An expression is a combination of numbers, variables, and operations.    This definition perhaps leads to more questions than it answers. After all, what are the precise, mathematical definitions of numbers, variables, and operations?  For us, in this book, numbers will always refer to real numbers . It is difficult to define real numbers precisely, but you are familiar with them nonetheless. These are numbers that correspond to positions on the number line. Positive real numbers correspond to lengths. All real numbers are positive, negative, or zero. Real numbers include whole numbers, fractions, and irrational numbers. Irrational numbers, like and , are numbers that can't be written as ratios of whole numbers, and their decimal representations are nonterminating and nonrepeating.  A variable is a letter or symbol that is used to represent an unknown or unspecified number. There is some nuance here. In an equation, the variable is unknown and the goal will be to solve for all possible values of the variable that make the equation true. In function notation, the variable is unspecified, and we will replace the variable with various numbers and see how the output of the function changes.  An operation is a rule for transforming one or more numbers into a new number. Most operations act on two numbers, and examples of operations include addition, subtraction, multiplication, and division . Importantly, equality ( ) is not an operation, but instead something called a relation . So, while and are examples of expressions, the equation is not an expression.  Equation   An equation is statement that the values of two mathematical expressions are equal.    An equation might always be true, like the equations and , or they might be conditionally true, such as the equation , which is only true if or . It is also possible for an equation to always be false, such as the equation .  Inequality   An inequality is a statement that compares two expressions using one of the following symbols: (less than), (less than or equal to), (greater than), (great than or equal to), or (not equal).    Just like equations, an inequality might be always true, conditionally true, or always false. For example, the inequality is true for every real number because the square of a real number is always zero or positive. On the other hand, the inequality is only true if the number is between 0 and 1.   Notes about inequalities     The statement means that the expression is positive. On the other hand, the statement means that the expression is negative.    Geometrically, saying means that the number is to the left of the number on the number line, while the statement means that is to the right of on the number line.    Every inequality written using \" \" (or \" \") can be rewritten using \" \" (or \" \") and vice versa. For example, the inequality is equivalent to . By the way, can you tell if these inequalities are true or false? If you're not sure, try using a number line.      Set   A set is a collection of numbers (or other mathematical objects).    Think of a set as a box that you can store numbers in. The numbers stored in a set are called the elements of that set. For example, the set containing the numbers 1, 3, and 5 would be written as . This is a finite set containing three elements. On the other hand, the set of even integers would contain every multiple of 2. This set could be written as , where the dots indicate that the pattern would continue in both directions. This is an infinite set, because no matter how many even numbers we list inside the brackets, we can never list them all. We will not spend much time on sets in these courses. If you are interested in learning more about set theory, you should take our Introduction to Mathematical Thinking course.  "
 },
 {
   "id": "def-expression",
@@ -24,8 +24,8 @@ var ptx_lunr_docs = [
   "url": "sec-rev-def.html#def-expression",
   "type": "Definition",
   "number": "1.1.1",
-  "title": "",
-  "body": "  An expression is a combination of numbers, variables, and operations.   "
+  "title": "Expression.",
+  "body": "Expression   An expression is a combination of numbers, variables, and operations.   "
 },
 {
   "id": "sec-rev-def-5",
@@ -60,8 +60,8 @@ var ptx_lunr_docs = [
   "url": "sec-rev-def.html#def-equation",
   "type": "Definition",
   "number": "1.1.2",
-  "title": "",
-  "body": "  An equation is statement that the values of two mathematical expressions are equal.   "
+  "title": "Equation.",
+  "body": "Equation   An equation is statement that the values of two mathematical expressions are equal.   "
 },
 {
   "id": "def-inequality",
@@ -69,8 +69,62 @@ var ptx_lunr_docs = [
   "url": "sec-rev-def.html#def-inequality",
   "type": "Definition",
   "number": "1.1.3",
+  "title": "Inequality.",
+  "body": "Inequality   An inequality is a statement that compares two expressions using one of the following symbols: (less than), (less than or equal to), (greater than), (great than or equal to), or (not equal).   "
+},
+{
+  "id": "sec-rev-def-12",
+  "level": "2",
+  "url": "sec-rev-def.html#sec-rev-def-12",
+  "type": "Remark",
+  "number": "1.1.4",
+  "title": "Notes about inequalities.",
+  "body": " Notes about inequalities     The statement means that the expression is positive. On the other hand, the statement means that the expression is negative.    Geometrically, saying means that the number is to the left of the number on the number line, while the statement means that is to the right of on the number line.    Every inequality written using \" \" (or \" \") can be rewritten using \" \" (or \" \") and vice versa. For example, the inequality is equivalent to . By the way, can you tell if these inequalities are true or false? If you're not sure, try using a number line.     "
+},
+{
+  "id": "def-set",
+  "level": "2",
+  "url": "sec-rev-def.html#def-set",
+  "type": "Definition",
+  "number": "1.1.5",
+  "title": "Set.",
+  "body": "Set   A set is a collection of numbers (or other mathematical objects).   "
+},
+{
+  "id": "sec-rev-def-14",
+  "level": "2",
+  "url": "sec-rev-def.html#sec-rev-def-14",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
   "title": "",
-  "body": "  An inequality is a statement that compares two expressions using one of the following symbols: (less than), (less than or equal to), (greater than), (great than or equal to), or (not equal).   "
+  "body": "elements "
+},
+{
+  "id": "sec-interval-notation",
+  "level": "1",
+  "url": "sec-interval-notation.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Interval Notation",
+  "body": " Interval Notation  Whe we solve equations, our answer is usually a single number or a list of numbers. However, when we are solving inequalities, our answers will be ranges of numbers called intervals .  We need a good way to describe these intervals. There are three methods that will be useful for us. For instance, if I want to describe the set of numbers between 1 and 3, including 1 and excluding 3, then I can describe this using the inequality I can also describe this set visually, by highlighting the numbers on a number line. When doing this, we will need to be careful with the endpoints. We will use a closed (i.e. a filled-in circle) if the endpoint is included, and we will use an open circle if the endpoint is not included. So our example of the numbers between 1 and 3, including 1 and excluding 3, would look like this:  The use of closed and open circles here is not accidental. A closed interval is an interval that includes its both of its endpoints, and an open interval is an interval that excludes both of its endpoints. An interval like the example above, which includes one endpoint and excludes the other, is neither open nor closed. Sometimes this type of interval is class half-open or half-closed .  "
+},
+{
+  "id": "sec-interval-notation-2",
+  "level": "2",
+  "url": "sec-interval-notation.html#sec-interval-notation-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "intervals "
+},
+{
+  "id": "sec-interval-notation-4",
+  "level": "2",
+  "url": "sec-interval-notation.html#sec-interval-notation-4",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "closed interval open interval half-open half-closed "
 },
 {
   "id": "sec-def-function",
